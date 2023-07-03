4229bf3fe5f36f7faad90b12c89ce02afa32f034
tadamcz <tadamczewskipublic@gmail.com>
2023-07-03 16:03:42 +0100

    stop silently swallowing so many AttributeErrors

--- rvtools/fam.py
+++ rvtools/fam.py
@@ -7,25 +7,28 @@ from rvtools.dists.gen.certainty import Certainty
 def is_frozen_norm(obj):
     """Returns ``True`` if and only if ``obj`` is a frozen normal distribution."""
     try:
-        return isinstance(obj.dist, scipy.stats._continuous_distns.norm_gen)
+        dist = obj.dist
     except AttributeError:
         return False
+    return isinstance(dist, scipy.stats._continuous_distns.norm_gen)
 
 
 def is_frozen_lognorm(obj):
     """Returns ``True`` if and only if ``obj`` is a frozen lognormal distribution."""
     try:
-        return isinstance(obj.dist, scipy.stats._continuous_distns.lognorm_gen)
+        dist = obj.dist
     except AttributeError:
         return False
+    return isinstance(dist, scipy.stats._continuous_distns.lognorm_gen)
 
 
 def is_frozen_beta(obj):
     """Returns ``True`` if and only if ``obj`` is a frozen beta distribution."""
     try:
-        return isinstance(obj.dist, scipy.stats._continuous_distns.beta_gen)
+        dist = obj.dist
     except AttributeError:
         return False
+    return isinstance(dist, scipy.stats._continuous_distns.beta_gen)
 
 
 def is_frozen_bernoulli(obj):
@@ -33,22 +36,25 @@ def is_frozen_bernoulli(obj):
     Returns ``True`` if and only if ``obj`` is a frozen Bernoulli distribution.
     """
     try:
-        return isinstance(obj.dist, scipy.stats._discrete_distns.bernoulli_gen)
+        dist = obj.dist
     except AttributeError:
         return False
+    return isinstance(dist, scipy.stats._discrete_distns.bernoulli_gen)
 
 
 def is_frozen_certainty(obj):
     """Returns ``True`` if and only if ``obj`` is a frozen certainty distribution."""
     try:
-        return isinstance(obj.dist, Certainty)
+        dist = obj.dist
     except AttributeError:
         return False
+    return isinstance(dist, Certainty)
 
 
 def is_frozen_pert(obj):
     """Returns ``True`` if and only if ``obj`` is a frozen PERT distribution."""
     try:
-        return isinstance(obj.dist, betapert.PERT)
+        dist = obj.dist
     except AttributeError:
         return False
+    return isinstance(dist, betapert.PERT)
